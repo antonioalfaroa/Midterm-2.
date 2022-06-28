8ce4f62731d4b0a1b6e025c28426535f094f8a0b
Antonio <78964462+antonioalfaroa@users.noreply.github.com>
2022-06-28 19:16:41 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+T interpolSearch(T& val, T* array, int max){
+  
+  for(int i=0;i<max;i++){
+    if(val < array[0] || val > array[max-1]){
+      return -1;
+    }
+    if(array[i]==val){
+      return i;
+    }
+  }
+}
+
+int main() {
+  int max = 10;
+  int X[] = {8, 6, 9, 8, 11, 3, 6};
+  int value = 8;
+  double interpol = interpolSearch(value, &X[1], max);
+  cout<<interpol<<endl;
+}
